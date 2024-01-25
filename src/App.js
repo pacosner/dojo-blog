90afa70a3dd9d97e10aa24197dbe5ff8299db067
Patrick Cosner <patrick@Patricks-MBP-M1.local>
2024-01-24 21:12:55 -0500

    up to lesson 13

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
+
+  const title = 'Welcome to the new blog';
+  const likes = '50';
+  const person = { name: 'yoshi', age: 30};
+
+  const link = "https://google.com";
+
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>App Component</h1>
+        <Home />
       </div>
     </div>
   );
